fa95f704e991f31542e49cd95dc1088372934f83
YuHuro <ayubovmagomed999@gmail.com>
2023-09-23 12:46:08 +0300

    update

---
+++ JavaS.js
@@ -0,0 +1,12 @@
+const nameInput = document.querySelector('#name-input');
+const mailInput = document.querySelector('#mail-input')
+const numInput = document.querySelector('#num-input')
+const confirmForm = document.querySelector('#confirm-form')
+
+function checkName(){
+    var tempName = nameInput.value;
+    if(tempName.length < 2){
+        nameInput.style.borderColor = 'red'
+    }
+}
+confirmForm.addEventListener('click', checkName)
