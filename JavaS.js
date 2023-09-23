c5d369b4ae840b61c8bff4dd9eb1cd11caaa2961
YuHuro <128969811+YuHuro@users.noreply.github.com>
2023-09-23 14:33:43 +0300

    Update JavaS.js

--- JavaS.js
+++ JavaS.js
@@ -3,7 +3,8 @@ const mailInput = document.querySelector('#mail-input')
 const numInput = document.querySelector('#num-input')
 const confirmForm = document.querySelector('#confirm-form')
 
-function checkName(){
+function checkName(ev){
+    ev.preventDefautlt()
     var tempName = nameInput.value;
     if(tempName.length < 2){
         nameInput.style.borderColor = 'red'
